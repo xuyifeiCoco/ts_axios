6f5966a2549724ec8ea3aba6dcbf73c9f6c546af
xuyifeiCoco <1227162950@qq.com>
2019-09-15 19:39:04 +0800

    密码存储

--- axios_ts_demo/src/index.ts
+++ axios_ts_demo/src/index.ts
@@ -1,6 +1,6 @@
 import { AxiosRequestConfig } from './types/index'
 import xhr from './xhr'
 function axios(config: AxiosRequestConfig): void {
-  xhr(config)
+  xhr(config) //密码存储
 }
 export default axios
