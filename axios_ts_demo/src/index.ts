5bcc570f5403d4e10f032f7d8be7b21f73876375
xuyifeiCoco <1227162950@qq.com>
2019-09-15 19:20:19 +0800

    发多少

--- axios_ts_demo/src/index.ts
+++ axios_ts_demo/src/index.ts
@@ -1,6 +1,6 @@
 import { AxiosRequestConfig } from './types/index'
 import xhr from './xhr'
 function axios(config: AxiosRequestConfig): void {
-  xhr(config)
+  xhr(config) //测试
 }
 export default axios
